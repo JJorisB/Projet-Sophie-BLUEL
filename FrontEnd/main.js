6ef3e003ac5d2ea63edd78cf22fc5bd8293103c9
JJorisB <jorysbilan@gmail.com>
2024-07-02 14:31:50 +0200

    First commit

---
+++ FrontEnd/main.js
@@ -0,0 +1,49 @@
+const gallery = document.querySelector(".gallery");
+let works = [];
+let categories = [];
+
+const getWorks = async () => {
+  const response = await fetch("http://localhost:5678/api/works");
+  console.log(response);
+
+  works = await response.json();
+  createGallery(works);
+};
+
+const getCategory = async () => {
+  const response = await fetch("http://localhost:5678/api/categories");
+
+  categories = await response.json();
+  console.log(categories);
+  filterWorks();
+};
+
+const filterWorks = () => {
+  const filterContainer = document.querySelector("#filters");
+  const allButton = document.createElement("button");
+  allButton.innerHTML = "Tous";
+  allButton.setAttribute("value", "all");
+  allButton.addEventListener("click", () => {
+    gallery.innerHTML = "";
+    createGallery(works);
+  });
+
+  filterContainer.appendChild(allButton);
+
+  categories.forEach((category) => {
+    const button = document.createElement("button");
+    button.innerHTML = category.name;
+    button.setAttribute("value", category.id);
+    filterContainer.appendChild(button);
+
+    button.addEventListener("click", () => {
+      const value = button.getAttribute("value");
+      const filteredWorks = works.filter((work) => work.categoryId == value);
+      gallery.innerHTML = "";
+      createGallery(filteredWorks);
+    });
+  });
+};
+
+getCategory();
+getWorks();
